caea05209347502843ea8ead23bd768fc0ccbc6e
carloslfu <carloslfu@gmail.com>
2015-03-02 21:30:44 -0500

    added level attr and paletteColor

--- core/BB.js
+++ core/BB.js
@@ -7,6 +7,7 @@ BB.Object = function(type) {
   this.type_ = type;
   this.children = [];
   this.nested = false;
+  this.level = 0; //level of nesting 0 - main Object
 };
 
 BB.Object.prototype.add = function(type, name, options) {
@@ -15,6 +16,7 @@ BB.Object.prototype.add = function(type, name, options) {
       break;
     case 'Workspace':
       this.children.push(new BB.Workspace(name, this, options));
+      this.children[this.children.length-1].level = this.level + 1;
       return this.children[this.children.length-1];
   }
 };
--- main.js
+++ main.js
@@ -4,8 +4,18 @@ var main = function() {
 	mainWorkspace.render();
 	var nested1 = mainWorkspace.add('Workspace', 'nested1', {width : 300, height: 300});
 	nested1.render();
-	var nested11 = nested1.add('Workspace', 'nested11', {width : 30, height: 30});
+	var nested11 = nested1.add('Workspace', 'nested11', {width : 100, height: 100});
 	nested11.render();
-	var nested2 = mainWorkspace.add('Workspace', 'nested2', {width : 300, height: 300, x: 50, y: 200});
+	var nested12 = nested1.add('Workspace', 'nested12', {width : 100, height: 100});
+	nested12.render();
+	var nested111 = nested11.add('Workspace', 'nested111', {width : 30, height: 30});
+	nested111.render();
+	var nested2 = mainWorkspace.add('Workspace', 'nested2', {width : 300, height: 300, x: 350, y: 50});
 	nested2.render();
+	var nested21 = nested2.add('Workspace', 'nested21', {width : 100, height: 100});
+	nested21.render();
+	var nested22 = nested2.add('Workspace', 'nested22', {width : 100, height: 100});
+	nested22.render();
+	var nested211 = nested21.add('Workspace', 'nested211', {width : 30, height: 30});
+	nested211.render();
 }
