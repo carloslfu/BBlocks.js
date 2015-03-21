d5ce02f452af739de086b10301b17ac9cc0c5ddc
carloslfu <carloslfu@gmail.com>
2015-03-21 01:56:38 -0500

    animation efect in test_dev block
    on pointerdown in blue rect of test_dev block, fires an animation

--- blocks/custom.js
+++ blocks/custom.js
@@ -31,7 +31,7 @@ test_blocks.test_dev = {
     var rect2 = this.workspace.root.rect(100, 100).fill('#1d028e').dmove(6, 6);
     group2.add(margin2);
     group2.add(rect2);
-    this.appendField(new BB.FieldText('hello! \n this is a text field', this));
+    this.appendField(new BB.FieldText('Click or touch\nthe blue rect', this));
     this.appendField(new BB.FieldSvg(group2, this));
   },
   attach: function(){
@@ -39,12 +39,15 @@ test_blocks.test_dev = {
     var this_ = this;
     // TODO: allows animate rotation
     var animation = function(){
-      this_.animate(1000).rotate(50).scale(1.4);
+      PolymerGestures.removeEventListener(this_.fields[1].root.node, 'down', animation);
+      this_.animate(1000).rotate(15).scale(1.2);
       setTimeout(function(){
         this_.animate(1000).rotate(0).scale(1);
+        setTimeout(function(){
+          PolymerGestures.addEventListener(this_.fields[1].root.node, 'down', animation);
+        }, 1000);
       },1000);
     }
-    animation();
-    setInterval(animation, 4000);
+    PolymerGestures.addEventListener(this.fields[1].root.node, 'down', animation);
   }
 };
