dfa8960e1f2c2839e3a2e5584ee8dde111ca6765
Carlos Galarza <carloslfu@gmail.com>
2015-06-10 11:51:08 -0500

    Playground test code changed

--- tests/main.js
+++ tests/main.js
@@ -1,16 +1,10 @@
-var mainWorkspace, nested1, nested11, nested12, nested121, nested2, nested21, nested22, nested221, block1, block2, block3, block4;
+var mainWorkspace, nested1, nested11, nested12, nested121, nested2, nested21, nested22, nested221, block1, block2, block3, block4, blockBlue;
 var main = function() {
   mainWorkspace = new BB.Workspace('main', BB.WorkspaceBasic, 'mainWorkspaceDiv');
-  nested1 = mainWorkspace.addWorkspace('nested1', BB.WorkspaceBasic, {title: 'Nested1', width : 300, height: 300, x: 10, y: 10});
-  nested11 = nested1.addWorkspace('nested11', BB.WorkspaceBasic, {title: 'Nested11', width : 100, height: 100, x: 10, y: 10});
-  nested12 = nested1.addWorkspace('nested12', BB.WorkspaceBasic, {title: 'Nested12', width : 100, height: 100, x: 20, y: 20});
-  nested121 = nested12.addWorkspace('nested121', BB.WorkspaceBasic, {title: 'Nested121', width : 30, height: 30, x: 10, y: 10});
-  nested2 = mainWorkspace.addWorkspace('nested2', BB.WorkspaceBasic, {title: 'Nested2', width : 300, height: 300});
-  nested21 = nested2.addWorkspace('nested21', BB.WorkspaceBasic, {title: 'Nested21', width : 100, height: 100, x: 10, y: 10});
-  nested22 = nested2.addWorkspace('nested22', BB.WorkspaceBasic, {title: 'Nested22', width : 100, height: 100, x: 20, y: 20});
-  nested221 = nested22.addWorkspace('nested221', BB.WorkspaceBasic, {title: 'Nested221', width : 30, height: 30, x: 10, y: 10});
-  nested2211 = nested221.addWorkspace('nested2211', BB.WorkspaceBasic, {title: 'Nested2211', width : 30, height: 30, x: 10, y: 10});
-  nested22111 = nested2211.addWorkspace('nested22111', BB.WorkspaceBasic, {title: 'Nested22111', width : 30, height: 30, x: 10, y: 10});
+  nested1 = mainWorkspace.addWorkspace('nested1', BB.WorkspaceBasic, {title: 'Nested1', width : 300, height: 400, x: 350, y: 40});
+  nested2 = mainWorkspace.addWorkspace('nested2', BB.WorkspaceBasic, {title: 'Nested2', width : 300, height: 400});
+  nested21 = nested2.addWorkspace('nested21', BB.WorkspaceBasic, {title: 'Nested21', width : 210, height: 140});
+
   //BB.Workspace.prototype.colorPalette = BB.colorPalettes.workspace.dark;
   //BB.Block.prototype.colorPalette = BB.colorPalettes.block.dark;
   // alternate color palette for workspace backgrounds
@@ -18,6 +12,7 @@ var main = function() {
     this.bgColor = this.colorPalette.background[(this.level % 2 == 1) ? 'nested' : 'main'];
     this.background.fill(this.bgColor);
   };
+  // Main Workspace
   mainWorkspace.render();
   block1 = mainWorkspace.addBlock('block1', test_blocks.test_dev);
   block1.render();
@@ -25,19 +20,10 @@ var main = function() {
   block2 = mainWorkspace.addBlock('block2', example_blocks.example);
   block2.render();
   block2.move(50, 170);
-  block3 = nested2.addBlock('block3', test_blocks.test);
-  block3.render();
-  block3.move(100, 100);
-  block6 = mainWorkspace.addBlock('blockBlue', test_blocks.test);
-  block6.render();
-  block6.setColor('#2870b7');
-  block6.move(30, 20);
-  nested2.move(350,50);
-  block4 = nested2.addBlock('block4', example_blocks.example);
-  block4.render();
-  nested2.rotate(0);
-  nested2.rotate(45);
-  block4.move(50, 170);
+  blockBlue = mainWorkspace.addBlock('blockBlue', test_blocks.test);
+  blockBlue.render();
+  blockBlue.setColor('#2870b7');
+  blockBlue.move(30, 20);
   //field button fires animation
   block2.fields[5].ondown = block1.methods.animation;
   block1.methods.animationStart = function () {
@@ -46,10 +32,22 @@ var main = function() {
   block1.methods.animationEnd = function () {
     block2.fields[5].ondown = block1.methods.animation;
   }
-  
-  // nesting blocks performance test
-  for (var i = 1; i < 0; i++) {
-  //  nested2211.addBlock('block' + i, example_blocks.example).render();
-  }
+  // Nested workspace 1
+  block3 = nested1.addBlock('block3', test_blocks.test_dev);
+  block3.render();
+  block3.rotate(0);
+  block3.move(20, 20);
+  block4 = nested1.addBlock('block4', example_blocks.example);
+  block4.render();
+  block4.move(20, 160);
+  // Nested workspace 2
+  nested2.move(670,40);
+  block6 = nested2.addBlock('block5', example_blocks.example);
+  block6.render();
+  block6.move(50, 165);
+  // Nested workspace 2 - 1
+  nested21.move(30,30).rotate(15);
+  block5 = nested21.addBlock('block6', test_blocks.test);
+  block5.render();
+  block5.move(50, 30);
 };
-
